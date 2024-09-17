a7ae773ae2babd69c39d35a540b4e91c452f46e9
nikhithak <knik01012001@gmail.com>
2024-09-17 18:47:54 -0500

    second commit: Created Item and ItemRepository

---
+++ src/main/java/com/model/Item.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+/**
+ *
+ * @author nikhi
+ */
+@Entity
+@Table
+public class Item {
+
+}
